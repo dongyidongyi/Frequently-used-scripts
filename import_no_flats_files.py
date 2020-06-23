84cbfffc22feb871d2d37116947f2e82175cc2aa
YD <dongyidongyi@users.noreply.github.com>
2020-06-23 17:35:15 +0200

    Update and rename import excel files to import_no_flats_files.py

--- import_no_flats_files.py
+++ import_no_flats_files.py
@@ -9,3 +9,21 @@ df2 = xls.parse(1, usecols=[0], skiprows=[0], names=['Country'])
 
 # Print the head of the DataFrame df2
 print(df2.head())
+
+
+# Import hdf5 files
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = 'LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
